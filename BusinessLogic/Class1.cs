c06b54b6187be5c45b8c402faa89f841d06f0da1
Brian Stiner <Brian.Stiner@revature.net>
2021-10-06 18:04:50 -0400

    10/6/2021 Added class variable in Models

--- BusinessLogic/Class1.cs
+++ BusinessLogic/Class1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 
+/* The logic to perform CRUD operations on the models.
+Contains repository specific logic on storing and accessing data
+*/
+
 namespace BusinessLogic
 {
     public class Class1
--- DataAccessLogic/Class1.cs
+++ DataAccessLogic/Class1.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 
+/* The logic to perform CRUD operations on the models.
+Contains repository specific logic on storing and accessing data
+*/
 namespace DataAccessLogic
 {
     public class Class1

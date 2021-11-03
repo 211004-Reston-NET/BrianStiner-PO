969c7ddbd148dfecf4d9d962d6a181a0976e5e27
Brian Stiner <Brian.Stiner@revature.net>
2021-11-02 20:50:55 -0400

    comments

--- UserInterface/Main/CheckoutMenu.cs
+++ UserInterface/Main/CheckoutMenu.cs
@@ -20,23 +20,23 @@ namespace UserInterface{
             var c = Current.customer;
             var s = Current.storefront;
 
-            // Store buying from distributor
+            // Store buying All orders from distributor and adding to inventory
             if(s.StoreOrders.Count > 0){
                 Builder.ResetPause(s.ToStringList());
                 foreach(Order o in  s.StoreOrders){
                     if(o.Active){
                     foreach(LineItem li in o.OrderLineItems){
                         s.Expenses += li.Total*.7M;                                                         //Stores get a discount from the distributor
-                        if(s.StoreLineItems.Find(x => x.ProductId == li.ProductId) != null){                //does it exist
-                        s.StoreLineItems.Find(x => x.ProductId == li.ProductId).Quantity += li.Quantity;    //add if it does
-                        }else{s.StoreLineItems.Add(li);}                                                    //add if it doesn't
+                        if(s.StoreLineItems.Find(x => x.ProductId == li.ProductId) != null){                //does it exist?
+                        s.StoreLineItems.Find(x => x.ProductId == li.ProductId).Quantity += li.Quantity;    //add if it does by merging
+                        }else{s.StoreLineItems.Add(li);}                                                    //add if it doesn't by adding
                     }
                     }   
                     o.Active = false;
                     Builder.Add($"Order {o.Id} with {o.OrderLineItems.Count} items complete.");     
                 } 
             }
-            // Customer buying from store
+            // Customer buying from store, increasing store revanue, decreasing store inventory, and increasing customer's totalspent
             bool orderSuccess = true;
             if(c.CustomerOrders.Count > 0){
                 Builder.ResetPause(c.ToStringList());
